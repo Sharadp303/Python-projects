a962ef86e7e337d2eecb18134b3e0117875e3292
Sharad patel <sharad.patels03@gmail.com>
2023-07-02 20:52:33 +0530

    upd

--- Robo speaker/main.py
+++ Robo speaker/main.py
@@ -6,7 +6,7 @@ while(True):
     print("Welocome to RoboSpeaker 1.0 Created by Sharad")
     x=input("What do you want me to speak: ")
     if(x=='q'):
-        speaker.say("Bye Bye Have a good day")
+        speaker.say("Bye Bye")
         speaker.runAndWait()
         break
     
